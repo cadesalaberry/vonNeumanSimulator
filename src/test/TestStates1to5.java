46e13b57b33822d975c8c5cca8f7e771b9d87c0b
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-10-17 14:38:25 -0400

    Mod: Moved helpers code into Routine.

--- src/test/TestStates1to5.java
+++ src/test/TestStates1to5.java
@@ -29,58 +29,7 @@ public class TestStates1to5 {
 		Computer computer = new Computer(test);
 		Bus b = computer.bus;
 
-		routinesAreMatching(test, xpct);
+		assertTrue(Routine.areMatching(test, xpct));
 	}
 
-	public boolean routinesAreMatching(String[] testStr, String[][] xpected) {
-
-		Computer computer = new Computer(testStr);
-		Bus b = computer.bus;
-		boolean passed = true;
-
-		for (int i = 0; i < xpected.length && passed; i++) {
-			computer.simulate(i);
-			passed = busStateIsMatching(b, xpected[i]);
-		}
-
-		return passed;
-	}
-
-	public boolean busStateIsMatching(Bus b, String[] xpected) {
-
-		String addressStr = Integer.toString(b.address);
-
-		String[] output = { addressStr, b.command, b.data };
-
-		for (int i = 0; i < output.length; i++) {
-			if (xpected[i] == null && output[i] == null) {
-
-			} else if (!xpected[i].equals(output[i])) {
-				System.out.println("Error@" + i
-						+ ": bus state is not matching expected value."
-						+ "\nHas Found: " + output[i] + "\nExpecting: "
-						+ xpected[i]);
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public boolean printRoutine(String[] testStr) {
-
-		Computer computer = new Computer(testStr);
-		Bus b = computer.bus;
-		boolean passed = true;
-
-		System.out.print("{");
-		for (int i = 0; i < 100 && passed; i++) {
-			computer.simulate(i);
-			System.out.print("{ \"" + b.address);
-			System.out.print("\", \"" + b.command);
-			System.out.print("\", \"" + b.data + "\" }\n");
-		}
-		System.out.print("}");
-
-		return passed;
-	}
 }
