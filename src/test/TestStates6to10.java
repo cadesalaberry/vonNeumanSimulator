f5d444e7006e7d81e7bb072e881749adf221dabf
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-10-17 12:47:28 -0400

    Add: Tests structure.

---
+++ src/test/TestStates6to10.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStates6to10 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
